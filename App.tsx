e83a1cd96e54d2ae7ba3b5338f56c1b372602f73
neilson <joseneilson25@gmail.com>
2022-08-06 00:36:36 -0300

    assets + NativeBase

--- App.tsx
+++ App.tsx
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SignIn } from "./src/screens/SignIn";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,9 @@
-function SignIn(){
-    
+import { View, Text } from "react-native";
+
+export function SignIn() {
+    return (
+        <View>
+            <Text>OLa mundo</Text>
+        </View>
+    );
 }
