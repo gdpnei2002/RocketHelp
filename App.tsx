cb4f70a8ac433fc5f45686667f1f8936cb1bf616
neilson <joseneilson25@gmail.com>
2022-08-06 06:13:51 -0300

    loading... (spinner)

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { SignIn } from "./src/screens/SignIn";
 import { NativeBaseProvider } from "native-base";
+import { THEME } from "./src/styles/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsloaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      {fontsloaded ? <SignIn /> : <Loading/>}
     </NativeBaseProvider>
   );
 }
